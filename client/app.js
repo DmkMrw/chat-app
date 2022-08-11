672d41d5eb9fe5315bd615a9c2601dbfd653b0a7
Dominik Mrówka <dmrowka7@gmail.com>
2022-08-11 12:05:30 +0200

    Add function addMessage

--- client/app.js
+++ client/app.js
@@ -31,6 +31,20 @@ const sendMessage = (e) => {
   };
 };
 
+const addMessage = (author, content) => {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if (author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+  messagesList.appendChild(message);
+}
+
 
 
 
